ec0a824ac9290415ce95645a88c84ea170748c62
larryhou <larryhou@foxmail.com>
2021-02-23 14:09:15 +0800

    Add site deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SITE_DEPLOY_DIR=$(dirname $0)/../${DEPLOY_DIR_NAME:-'abtool-docs'}
+if [ ! -d ${SITE_DEPLOY_DIR} ]
+then
+    mkdir -p ${SITE_DEPLOY_DIR}
+    pushd ${SITE_DEPLOY_DIR}
+    git clone -b site git@github.com:larryhou/abtool-gitbook.git .
+    popd
+fi
+
+rsync -av $(dirname $0)/_book/ ${SITE_DEPLOY_DIR}/
+pushd ${SITE_DEPLOY_DIR}
+git add . 
+git commit -a -m "deploy gitbook $(date '+%Y-%m-%d %H:%M:%S')"
+git push
+popd
