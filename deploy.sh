9c7609feacba8b2854d929086daef514a3c748d3
larryhou <larryhou@foxmail.com>
2021-02-24 10:21:11 +0800

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-SITE_DEPLOY_DIR=$(dirname $0)/../${DEPLOY_DIR_NAME:-'abtool-docs'}
+pushd $(dirname $0)
+gitbook pdf
+
+SITE_DEPLOY_DIR=../${DEPLOY_DIR_NAME:-'abtool-docs'}
 if [ ! -d ${SITE_DEPLOY_DIR} ]
 then
     mkdir -p ${SITE_DEPLOY_DIR}
     pushd ${SITE_DEPLOY_DIR}
     git clone -b site git@github.com:larryhou/abtool-gitbook.git .
-    popd
+    popd # SITE_DEPLOY_DIR
 fi
 
-rsync -av --exclude='/.git' --delete $(dirname $0)/_book/ ${SITE_DEPLOY_DIR}/
+rsync -av --exclude='/.git' --delete book.pdf ${SITE_DEPLOY_DIR}/
+rsync -av --exclude='/.git' --delete _book/ ${SITE_DEPLOY_DIR}/
 pushd ${SITE_DEPLOY_DIR}
 git add . 
 git commit -a -m "deploy gitbook $(date '+%Y-%m-%d %H:%M:%S')"
 git push
-popd
+popd # SITE_DEPLOY_DIR
+
+popd # abtool
